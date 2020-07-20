90f23c86222679235103b2b6909a83cc73765549
olgab <olgabresheva7@gmail.com>
2020-07-19 23:43:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counterByOne, setCounterByOne] = useState(0);
+    const [counterByTen, setCounterByTen] = useState(0);
+    const [counterByN, setCounterByN] = useState(0);
+
+    const increaseByOne = () => {
+        return setCounterByOne(counterByOne + 1);
+    }
+
+    const descreaseByOne = () => {
+        return setCounterByOne(counterByOne - 1);
+    }
+
+    const increaseByTen = () => {
+        return setCounterByTen(counterByTen + 10);
+    }
+
+    const descreaseByTen = () => {
+        return setCounterByTen(counterByTen - 10);
+    }
+
+    const getRandomInt = max => {
+        return Math.floor(Math.random() * Math.floor(max));
+    };
+
+    const increaseByN = () => {
+        return setCounterByN(counterByN + getRandomInt(11));
+    }
+
+    const descreaseByN = () => {
+        return setCounterByN(counterByN - getRandomInt(11));
+    }
+
+    const clearAll = () => {
+        setCounterByOne(0);
+        setCounterByTen(0);
+        setCounterByN(0);
+    }
+
+    return (
+        <div className="App">
+
+            <p/>
+            <button onClick={descreaseByOne}>Minus One</button>
+            <span>{counterByOne}</span>
+            <button onClick={increaseByOne}>Plus One</button>
+            <hr/>
+            <button onClick={descreaseByTen}>Minus Ten</button>
+            <span>{counterByTen}</span>
+            <button onClick={increaseByTen}>Plus Ten</button>
+            <hr/>
+            <button onClick={descreaseByN}>Minus N</button>
+            <span>{counterByN}</span>
+            <button onClick={increaseByN}>Plus N</button>
+            <hr/>
+            <button onClick={clearAll}>Reset</button>
+        </div>
+    );
 }
 
 export default App;
