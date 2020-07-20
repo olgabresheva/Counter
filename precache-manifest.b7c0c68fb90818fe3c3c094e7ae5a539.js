5f699d32216f1c49ad181510e5312e22682165e6
olgab <olgabresheva7@gmail.com>
2020-07-19 23:53:08 -0700

    Updates

---
+++ precache-manifest.b7c0c68fb90818fe3c3c094e7ae5a539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be80c6793be57321f80c2bb2c7a48f2e",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "37bbb7c82c679d0b8ff3",
+    "url": "/counter/static/css/main.3b62710d.chunk.css"
+  },
+  {
+    "revision": "cd21a04a0b99dce3ff5c",
+    "url": "/counter/static/js/2.4e75bd1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.4e75bd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37bbb7c82c679d0b8ff3",
+    "url": "/counter/static/js/main.99357920.chunk.js"
+  },
+  {
+    "revision": "f0efe914179fd241fe39",
+    "url": "/counter/static/js/runtime-main.60e3ae63.js"
+  }
+]);
